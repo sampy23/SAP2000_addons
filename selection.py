9173350c392cbba30fceaa23dde3de4773545d64
sampy23 <sbz5677@gmail.com>
2020-10-06 16:52:56 +0400

    generalised x and y section cut

--- selection.py
+++ selection.py
@@ -39,27 +39,24 @@ class App():
         # recreating frame_1
         self.master.frame_1 = tk.LabelFrame(root)
         self.master.frame_1.grid(row=0,column=0)
-        self.button_1 = tk.Button(self.master.frame_1,text = "X Sn cut",width=16,
-                    bg = 'pale green',activebackground = 'orange red',relief = 'raised',command = self.x_sncut)
-        self.button_2 = tk.Button(self.master.frame_1,text = "Y Sn cut",width=16,
+        self.button_1 = tk.Button(self.master.frame_1,text = "X/Y Sn cut",width=16,
                     bg = 'pale green',activebackground = 'orange red',relief = 'raised',command = self.y_sncut)
-        self.button_3 = tk.Button(self.master.frame_1,text = "Inclined Sn cut",width=16,
+        self.button_2 = tk.Button(self.master.frame_1,text = "Inclined Sn cut",width=16,
                     bg = 'pale green',activebackground = 'orange red',relief = 'raised',command = self.inclined_sncut)
-        self.button_4 = tk.Button(self.master.frame_1,text = "Mirror selection",width=16,
+        self.button_3 = tk.Button(self.master.frame_1,text = "Mirror selection",width=16,
                     bg = 'pale green',activebackground = 'orange red',relief = 'raised',command = self.mirror)
-        self.button_5 = tk.Button(self.master.frame_1,text = "Select similar",width=16,
+        self.button_4 = tk.Button(self.master.frame_1,text = "Select similar",width=16,
                     bg = 'pale green',activebackground = 'orange red',relief = 'raised',command = self.select_similar)
 
         self.button_1.grid(row = 0,column=0,padx=10,pady=10)
         self.button_2.grid(row = 0,column=1,padx=10,pady=10)
         self.button_3.grid(row = 1,column=0,padx=10,pady=10)
         self.button_4.grid(row = 1,column=1,padx=10,pady=10)
-        self.button_5.grid(row = 2,column=0,padx=10,pady=10)
         self.button_1.config(font=self.font_size)
         self.button_2.config(font=self.font_size)
         self.button_3.config(font=self.font_size)
         self.button_4.config(font=self.font_size)
-        self.button_5.config(font=self.font_size)
+
 
     def attach_to_instance(self):
         try:
@@ -181,42 +178,27 @@ class App():
                 matching.append(lab)
         return matching
         
-
-    def x_sncut(self):
-        file_name = self.attach_to_instance()
-        self.master.title(file_name)
-        # section cut, in Y plane (along x axis)
-        frame_info = self.SapModel.SelectObj.GetSelected()
-        object_types = frame_info[1] 
-        selected_labels = frame_info[2]
-        length = float(self.entry_set[1]) # an arbitrary number greater than length of building
-        max_z = float(self.entry_set[2])
-        for object_type,label in zip(object_types,selected_labels):
-            if object_type == 2: #  exclude joints
-                joints = self.SapModel.FrameObj.GetPoints(label)
-                point_1 = self.SapModel.PointObj.GetCoordCartesian(joints[0])[:-1]
-                point_2 = self.SapModel.PointObj.GetCoordCartesian(joints[1])[:-1]
-                self.SapModel.SelectObj.CoordinateRange(min(point_1[0],point_2[0])-length,max(point_1[0],point_2[0])+\
-                                                        length,min(point_1[1],point_2[1]),max(point_1[1],point_2[1])\
-                                                        ,-max_z,max_z,IncludeIntersections = False)
-        self.SapModel.View.RefreshView()
-
     def y_sncut(self):
         file_name = self.attach_to_instance()
         self.master.title(file_name)
-        # section cut, in X plane (along y axis)
+        # section cut, in X/Y plane 
         object_types = self.SapModel.SelectObj.GetSelected()[1] 
         selected_labels = self.SapModel.SelectObj.GetSelected()[2]
         length = float(self.entry_set[1]) # an arbitrary number greater than length of building
         max_z = float(self.entry_set[2])
         for object_type,label in zip(object_types,selected_labels):
-            if object_type == 2:
+            if object_type == 2: # to exclude points
                 joints = self.SapModel.FrameObj.GetPoints(label)
                 point_1 = self.SapModel.PointObj.GetCoordCartesian(joints[0])[:-1]
                 point_2 = self.SapModel.PointObj.GetCoordCartesian(joints[1])[:-1]
-                self.SapModel.SelectObj.CoordinateRange(min(point_1[0],point_2[0]),max(point_1[0],point_2[0]),\
-                                                min(point_1[1],point_2[1])-length,max(point_1[1],point_2[1])+length\
-                                                ,-max_z,max_z,IncludeIntersections = False)
+                if round(point_1[0],4) == round(point_2[0],4): #for y section cut
+                    self.SapModel.SelectObj.CoordinateRange(min(point_1[0],point_2[0]),max(point_1[0],point_2[0]),\
+                                                    min(point_1[1],point_2[1])-length,max(point_1[1],point_2[1])+length\
+                                                    ,-max_z,max_z,IncludeIntersections = False)
+                if round(point_1[1],4) == round(point_2[1],4): #for x section cut
+                    self.SapModel.SelectObj.CoordinateRange(min(point_1[0],point_2[0])-length,max(point_1[0],point_2[0])+length,\
+                                                    min(point_1[1],point_2[1]),max(point_1[1],point_2[1])\
+                                                    ,-max_z,max_z,IncludeIntersections = False)
         self.SapModel.View.RefreshView()
         
     def inclined_sncut(self):
